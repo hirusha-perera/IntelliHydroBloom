78f199e8bfc704a302de57217e896aa8ec48c722
iamhirusha <hirushaperera98@gmail.com>
2023-10-20 17:08:06 +0530

    device done

--- app/src/main/java/com/example/intellihydrobloom/DeviceFragment.java
+++ app/src/main/java/com/example/intellihydrobloom/DeviceFragment.java
@@ -1,48 +1,166 @@
 package com.example.intellihydrobloom;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.util.HashMap;
 
 public class DeviceFragment extends Fragment {
 
+    private static final String USB_PERMISSION = "com.yourapp.USB_PERMISSION";
+    private UsbCommunication usbCommunication;
+    private UsbManager usbManager;
+    private UsbDevice usbDevice;
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
-    public DeviceFragment() {
-
-    }
-
-    public static DeviceFragment newInstance(String param1, String param2) {
-        DeviceFragment fragment = new DeviceFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_device, container, false);
+        usbManager = (UsbManager) getContext().getSystemService(Context.USB_SERVICE);
+
+        usbDevice = getUsbDevice();  // Initialize usbDevice
+
+        Button addButton = view.findViewById(R.id.btn_device_save);
+
+        if(usbDevice != null) {
+            TextView connectionStatus = view.findViewById(R.id.txt_connection_status);
+            connectionStatus.setText("Connected");
+            connectionStatus.setTextColor(getResources().getColor(R.color.h_gradient_dark_green));
+        }
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUsbData();
+            }
+        });
+
+        checkUsbPermission();
+
+        return view;
+    }
+
+    private UsbDevice getUsbDevice() {
+        HashMap<String, UsbDevice> usbDevices = usbManager.getDeviceList();
+        for (UsbDevice device : usbDevices.values()) {
+            // Assuming your NodeMCU has a vendor ID of 1234 and a product ID of 5678
+            if (device.getVendorId() == 4292  && device.getProductId() == 60000 ) {
+                return device;
+            }
+        }
+        return null;
+    }
+
+    private void checkUsbPermission() {
+        if (usbDevice != null) {
+            if (usbManager.hasPermission(usbDevice)) {
+                // Permission already granted, initialize UsbCommunication
+                usbCommunication = new UsbCommunication(getContext(), usbDevice);
+            } else {
+                // Request permission
+                PendingIntent permissionIntent = PendingIntent.getBroadcast(getContext(), 0, new Intent(USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
+                usbManager.requestPermission(usbDevice, permissionIntent);
+            }
+        } else {
+            Toast.makeText(getContext(), "USB device not initialized", Toast.LENGTH_SHORT).show();
         }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    private void sendUsbData() {
+        // Get user input
+        EditText deviceNameField = getView().findViewById(R.id.txt_deviceName);
+        String deviceName = deviceNameField.getText().toString();
 
-        return inflater.inflate(R.layout.fragment_device, container, false);
+        EditText plantTypeField = getView().findViewById(R.id.txt_plantType);
+        String plantType = plantTypeField.getText().toString();
+
+        EditText plantSpeciesField = getView().findViewById(R.id.txt_plantSpecies);
+        String plantSpecies = plantSpeciesField.getText().toString();
+
+        EditText wifiSSIDField = getView().findViewById(R.id.txt_wifiSSID);
+        String wifiSSID = wifiSSIDField.getText().toString();
+
+        EditText wifiPasswordField = getView().findViewById(R.id.txt_wifiPassword);
+        String wifiPassword = wifiPasswordField.getText().toString();
+
+        // Construct data string to send
+        String dataString = deviceName + "," + plantType + "," + plantSpecies + "," + wifiSSID + "," + wifiPassword + "\n";
+        byte[] data = dataString.getBytes();
+        ProgressBar progressBar = getView().findViewById(R.id.data_sending_progressbar);
+        progressBar.setVisibility(View.VISIBLE);
+        // Send data over USB
+        if (usbCommunication != null) {
+            int result = usbCommunication.sendData(data);
+            if (result >= 0) {
+                Toast.makeText(getContext(), "Data sent successfully", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Failed to send data", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(getContext(), "USB not connected", Toast.LENGTH_SHORT).show();
+        }
+        progressBar.setVisibility(View.GONE);
+    }
+
+
+    private final BroadcastReceiver usbPermissionReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (device != null) {
+                            usbCommunication = new UsbCommunication(getContext(), usbDevice);
+                        }
+                    } else {
+                        Toast.makeText(getContext(), "USB permission denied", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }
+    };
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        IntentFilter permissionFilter = new IntentFilter(USB_PERMISSION);
+        permissionFilter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        permissionFilter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        getActivity().registerReceiver(usbPermissionReceiver, permissionFilter);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        getActivity().unregisterReceiver(usbPermissionReceiver);
+    }
+
+    @Override
+    public void onDestroy() {
+        if (usbCommunication != null) {
+            usbCommunication.closeConnection();
+        }
+        super.onDestroy();
     }
 }
